d9c98ca699f8e83b516cee635ed57a6bed17d22d
Mjohannesson <42094833+Mjohannesson@users.noreply.github.com>
2026-09-19 16:20:19 -0400

    Delete const.py

--- const.py
+++
@@ -1,7 +0,0 @@
-DOMAIN = "london_pollen"
-DEFAULT_NAME = "London Pollen"
-DEFAULT_URL = "https://www.theweathernetwork.com/en/city/ca/ontario/london/pollen"
-DEFAULT_SCAN_INTERVAL_MINUTES = 360
-CONF_URL = "url"
-CONF_SCAN_INTERVAL = "scan_interval"
-PLATFORMS = ["sensor"]
